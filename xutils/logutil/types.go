dc7609ab5920ad6d159d7734f861be33f76f3662
mervyn <mervyn1988@gmail.com>
2025-12-09 20:00:02 +0800

    fix

--- xutils/logutil/types.go
+++ xutils/logutil/types.go
@@ -4,7 +4,7 @@ type Config struct {
 	IntervalSec    int64  `json:"IntervalSec"`
 	Limit          int    `json:"Limit"`
 	DisableStmtLog bool   `json:"DisableStmtLog"`
-	NotifyChannel  string `json:"NotifyChannel"`
+	NotifyChannel  string `json:"NotifyChannel,optional"`
 	NotifyWebhook  string `json:"NotifyWebhook"`
 	NotifySecret   string `json:"NotifySecret"`
 }
