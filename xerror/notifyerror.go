1b0ae437926a4416815c456a28ed1df8e5679957
mervyn <mervyn1988@gmail.com>
2025-12-11 15:40:07 +0800

    fix

--- xerror/notifyerror.go
+++ xerror/notifyerror.go
@@ -21,6 +21,11 @@ func (e *NotifyError) Error() string {
 	return fmt.Sprintf("provider: %d, err_msg: %s", e.Provider, e.ErrMsg)
 }
 
+func (e *NotifyError) Wrap(err error) error {
+	e.ErrMsg = err.Error()
+	return e
+}
+
 func GetNotifyProvider(err error) int {
 	var ne *NotifyError
 	if errors.As(err, &ne) {
