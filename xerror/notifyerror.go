9b5f54e76f02e61dc160936c67cc7e082d1c0d0a
mervyn <mervyn1988@gmail.com>
2025-12-11 14:16:22 +0800

    fix notify error

---
+++ xerror/notifyerror.go
@@ -0,0 +1,23 @@
+package xerror
+
+import (
+	"errors"
+	"fmt"
+)
+
+type NotifyError struct {
+	Provider int
+	ErrMsg   string
+}
+
+func (e *NotifyError) Error() string {
+	return fmt.Sprintf("provider: %d, err_msg: %s", e.Provider, e.ErrMsg)
+}
+
+func GetNotifyProvider(err error) int {
+	var ne *NotifyError
+	if errors.As(err, &ne) {
+		return ne.Provider
+	}
+	return 0
+}
